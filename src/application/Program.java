518aacb36c8c54894e996006103bb54de7d699e8
Lucas Borges <developerlucasborges@gmail.com>
2025-08-18 20:43:33 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,12 @@
 package application;
 
+import boardname.Position;
+
 public class Program {
 	public static void main(String[] args) {
 		
-		System.out.println("HELLO WORLD!");
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
 }
+ 
