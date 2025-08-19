bae2d7b5513fc8b1e2e928dc8711afbb86847efe
Lucas Borges <developerlucasborges@gmail.com>
2025-08-18 21:47:49 -0300

    BoardException and defensive programming

---
+++ src/boardname/BoardException.java
@@ -0,0 +1,10 @@
+package boardname;
+
+public class BoardException extends RuntimeException {
+
+	private static final long serialVersion = 1L;
+	
+	public BoardException(String msg) {
+		super(msg);
+	}
+}
